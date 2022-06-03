a5ccb9c8c030b29375ff34ce672175bc032f4e49
techpril <priscila.teodorio2015@gmail.com>
2022-06-03 11:16:47 -0300

    Colocando modal no App

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
 
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-    function handleOpenNewTransactionModal() {
-        setIsNewTransactionModalOpen(true);
-    }
-
-    function handleCloseNewTransactionModal() {
-        setIsNewTransactionModalOpen(false);
-    }
-
+export function Header({onOpenNewTransactionModal} : HeaderProps) {
     return (
         <Container>
             <Content>
@@ -23,16 +14,10 @@ export function Header() {
                     <h1 className="title_logo">OftenMoney</h1>
                 </div>
                 
-                <button type="button" onClick={handleOpenNewTransactionModal}>
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
 
-                <Modal 
-                    isOpen={isNewTransactionModalOpen}
-                    onRequestClose={handleCloseNewTransactionModal}
-                >
-                    <h2>Cadastrar transação</h2>
-                </Modal>
             </Content>
             
         </Container>
