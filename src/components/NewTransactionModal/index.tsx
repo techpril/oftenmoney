37bc67d6a0db79617053e50d7d0603257d9e6c57
techpril <priscila.teodorio2015@gmail.com>
2022-06-07 19:34:26 -0300

    Estilizando button de tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import IncomeImg from '../../assets/income.svg'
+import OutcomeImg from '../../assets/outcome.svg'
+import { Container, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg'
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -22,6 +24,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 <h2>Cadastrar transação</h2>
                 <input type="text" placeholder='Título'/>
                 <input type="number" placeholder='Valor'/>
+
+                <TransactionTypeContainer>
+                    <button type='button'>
+                        <img src={IncomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+
+                    <button type='button'>
+                        <img src={OutcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+
+                </TransactionTypeContainer>
+
                 <input type="text" placeholder='Categoria'/>
 
                 <button type="submit">Cadastrar</button>
